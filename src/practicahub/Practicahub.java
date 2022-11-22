ca5be6fb2ab4ee985f7234ac74d0aada53e98f3f
carab <carab@LAPTOP-NCRIVJ1F>
2022-11-22 12:47:00 -0600

    Ejemplo de conexión

---
+++ src/practicahub/Practicahub.java
@@ -0,0 +1,18 @@
+
+package practicahub;
+
+/**
+ *
+ * @author Leesly, Segio, Daniel
+ */
+public class Practicahub {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Hola Leesly");
+        System.out.println("Hola Sergio");
+        System.out.println("Hola Daniel");
+    }
+    
+}
